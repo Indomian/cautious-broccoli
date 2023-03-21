e8cf7ecb12b10f8055b571da0c62302504a50412
Egor Bolgov <e.bolgov@semrush.com>
2023-03-22 01:38:22 +0200

    Try to fix problems

--- src/host/workerWorkaround.js
+++ src/host/workerWorkaround.js
@@ -1,3 +1,3 @@
 export function createWorker() {
-    new Worker(new URL('../worker/main.ts', import.meta.url), {type: 'module'});
+    return new Worker(new URL('../worker/main.ts', import.meta.url), {type: 'module'});
 }
